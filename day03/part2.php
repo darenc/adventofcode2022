b91c54e50fd0b9d55253fdf11ca2afc595316d87
darenc <dchandisingh@gmail.com>
2022-12-03 11:38:40 +0000

    Day 3 Part 2

---
+++ day03/part2.php
@@ -0,0 +1,28 @@
+<?php
+
+$backpacks = file('input.txt');
+
+$sumPriorities = 0;
+
+foreach ($backpacks as $k => $backpack) {
+    $backpacks[$k] = str_split(trim($backpack));
+}
+
+for ($elf = 0; $elf < count($backpacks); $elf += 3) {
+
+    $elfCommonItems = array_intersect(
+        $backpacks[$elf],
+        $backpacks[$elf+1],
+        $backpacks[$elf+2]
+    );
+
+    array_unique($elfCommonItems);
+    $commonItem = array_shift($elfCommonItems);
+
+    $charValue = ord($commonItem);
+    $priority = $charValue <= 90 ? $charValue - 38 : $charValue - 96;
+
+    $sumPriorities += $priority;
+}
+
+echo $sumPriorities;
